05a4319d5adfba7d789147ccc563c613b0108fa6
sankar053 <sankaraasn@gmail.com>
2020-11-29 18:34:29 +0530

    First Commit

---
+++ src/main/java/com/iii/amirutham/AmiruthamApiApplication.java
@@ -0,0 +1,13 @@
+package com.iii.amirutham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmiruthamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmiruthamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iii/amirutham/AmiruthamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iii.amirutham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmiruthamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
