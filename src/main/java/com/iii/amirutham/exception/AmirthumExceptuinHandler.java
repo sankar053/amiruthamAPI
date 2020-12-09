1e43b901a9ca3d1d3e5ae9aa7b351ca12fc33433
sankar053 <sankaraasn@gmail.com>
2020-12-09 22:42:02 +0530

    added immages

--- src/main/java/com/iii/amirutham/exception/AmirthumExceptuinHandler.java
+++ src/main/java/com/iii/amirutham/exception/AmirthumExceptuinHandler.java
@@ -2,9 +2,12 @@ package com.iii.amirutham.exception;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailAuthenticationException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,47 +18,69 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import com.iii.amirutham.dto.base.GenericResponse;
 
+
 @ControllerAdvice
 @RestController
 public class AmirthumExceptuinHandler extends ResponseEntityExceptionHandler {
+
+	@Autowired
+	private MessageSource messages;
+
 	@ExceptionHandler(Exception.class)
 	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) throws Exception {
-		ExceptionResponse exceptionResponse = new ExceptionResponse("500",ex.getMessage(), new Date(),
-				request.getDescription(false));
-
-		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+		logger.error("500 Status Code", ex);
+		final GenericResponse bodyOfResponse = new GenericResponse(
+				messages.getMessage("message.error", null, request.getLocale()), "InternalError");
+		return new ResponseEntity<>(bodyOfResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 
 	}
 
 	@ExceptionHandler(UserNotFoundException.class)
 	public final ResponseEntity<Object> handleNotFoundException(UserNotFoundException ex, WebRequest request)
 			throws Exception {
-		ExceptionResponse exceptionResponse = new ExceptionResponse("4000",ex.getMessage(), new Date(),
+		ExceptionResponse exceptionResponse = new ExceptionResponse("4000", ex.getMessage(), new Date(),
 				request.getDescription(false));
 
 		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NOT_FOUND);
 
 	}
-	
+
+	// 409
+	@ExceptionHandler({ UserAlreadyExistException.class })
+	public ResponseEntity<Object> handleUserAlreadyExist(final RuntimeException ex, final WebRequest request) {
+		logger.error("409 Status Code", ex);
+		final GenericResponse bodyOfResponse = new GenericResponse(
+				messages.getMessage("message.regError", null, request.getLocale()), "UserAlreadyExist");
+		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+	}
+
 	@ExceptionHandler(AmirthumCommonException.class)
 	public final ResponseEntity<Object> handleNotFoundException(AmirthumCommonException ex, WebRequest request)
 			throws Exception {
-		ExceptionResponse exceptionResponse = new ExceptionResponse("4001",ex.getMessage(), new Date(),
+		ExceptionResponse exceptionResponse = new ExceptionResponse("4001", ex.getMessage(), new Date(),
 				request.getDescription(false));
 
 		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.OK);
 
 	}
 
-	
-	
-	 @Override
-	    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex,
-	    		final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
-	        logger.error("400 Status Code", ex);
-	        final BindingResult result = ex.getBindingResult();
-	        final GenericResponse bodyOfResponse = new GenericResponse(result.getAllErrors(), "Invalid" + result.getObjectName());
-	        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
-	    }
+	// 500
+	@ExceptionHandler({ MailAuthenticationException.class })
+	public ResponseEntity<Object> handleMail(final RuntimeException ex, final WebRequest request) {
+		logger.error("500 Status Code", ex);
+		final GenericResponse bodyOfResponse = new GenericResponse(
+				messages.getMessage("message.email.config.error", null, request.getLocale()), "MailError");
+		return new ResponseEntity<>(bodyOfResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex,
+			final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+		logger.error("400 Status Code", ex);
+		final BindingResult result = ex.getBindingResult();
+		final GenericResponse bodyOfResponse = new GenericResponse(result.getAllErrors(),
+				"Invalid" + result.getObjectName());
+		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	}
 
 }
